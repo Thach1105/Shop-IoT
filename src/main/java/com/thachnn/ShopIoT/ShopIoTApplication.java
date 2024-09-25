3f80c0663acfdecdc5f1c03e96468fb576891694
thachnn <thach11052002@gmail.com>
2024-09-25 22:25:30 +0700

    .

---
+++ src/main/java/com/thachnn/ShopIoT/ShopIoTApplication.java
@@ -0,0 +1,13 @@
+package com.thachnn.ShopIoT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopIoTApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopIoTApplication.class, args);
+	}
+
+}
