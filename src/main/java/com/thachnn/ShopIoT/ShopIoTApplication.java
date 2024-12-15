c8cfd75d635a3ff95a90b5e41ab38f813da732ca
thachnn <thach11052002@gmail.com>
2024-12-15 20:14:44 +0700

    add annotation @EnableFeignClients

--- src/main/java/com/thachnn/ShopIoT/ShopIoTApplication.java
+++ src/main/java/com/thachnn/ShopIoT/ShopIoTApplication.java
@@ -2,8 +2,10 @@ package com.thachnn.ShopIoT;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class ShopIoTApplication {
 
 	public static void main(String[] args) {
