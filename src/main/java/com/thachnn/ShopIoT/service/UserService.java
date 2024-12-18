fb6df4b197fdc5bc26f3060b2e5e7b8a9d60dbd7
thachnn <thach11052002@gmail.com>
2024-12-18 21:15:18 +0700

    format code

--- src/main/java/com/thachnn/ShopIoT/service/UserService.java
+++ src/main/java/com/thachnn/ShopIoT/service/UserService.java
@@ -3,6 +3,7 @@ package com.thachnn.ShopIoT.service;
 import com.thachnn.ShopIoT.dto.request.ChangePasswordRequest;
 import com.thachnn.ShopIoT.dto.request.CreateUserRequest;
 import com.thachnn.ShopIoT.dto.request.UpdateUserRequest;
+import com.thachnn.ShopIoT.dto.response.UserResponse;
 import com.thachnn.ShopIoT.exception.AppException;
 import com.thachnn.ShopIoT.exception.ErrorApp;
 import com.thachnn.ShopIoT.mapper.UserMapper;
@@ -19,6 +20,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
@@ -33,67 +35,83 @@ public class UserService {
             UserMapper userMapper,
             RoleService roleService,
             UserRepository userRepository
-    ){
-        this.userRepository = userRepository;
-        this.userMapper = userMapper;
+    ) {
         this.passwordEncoder = passwordEncoder;
+        this.userMapper = userMapper;
         this.roleService = roleService;
+        this.userRepository = userRepository;
     }
 
-    public User create(CreateUserRequest request){
+    // Create user
+    public User create(CreateUserRequest request) {
+        if (userRepository.existsByUsername(request.getUsername())) {
+            throw new AppException(ErrorApp.USERNAME_EXISTED);
+        }
+        if (userRepository.existsByEmail(request.getEmail())) {
+            throw new AppException(ErrorApp.EMAIL_EXISTED);
+        }
+
         Role roleUSER = roleService.getByName("USER");
         User user = userMapper.toUser(request);
         user.setPassword(passwordEncoder.encode(request.getPassword()));
         user.setRole(roleUSER);
 
-        if(userRepository.existsByUsername(request.getUsername())) throw new AppException(ErrorApp.USERNAME_EXISTED);
-        if(userRepository.existsByEmail(request.getEmail())) throw new AppException(ErrorApp.EMAIL_EXISTED);
-
         return userRepository.save(user);
     }
 
+    // Retrieve all users
     @PreAuthorize("hasRole('ADMIN')")
-    public List<User> getAll(){
-        return userRepository.findAll();
+    public List<UserResponse> getAll() {
+        List<User> users = userRepository.findAll();
+        return users.stream().map(userMapper::toUserResponse).collect(Collectors.toList());
     }
 
+    // Retrieve user by ID
     @PreAuthorize("hasRole('ADMIN') or #id == principal.claims['data']['id']")
-    public User getById(Integer id){
+    public User getById(Integer id) {
         return userRepository.findById(id)
                 .orElseThrow(() -> new AppException(ErrorApp.USER_NOTFOUND));
-
     }
 
-    public User getByUsername(String username){
+    // Retrieve user by username
+    public User getByUsername(String username) {
         return userRepository.findByUsername(username)
                 .orElseThrow(() -> new AppException(ErrorApp.USER_NOTFOUND));
     }
 
-    public User getByEmail(String email){
-        return  userRepository.findByEmail(email)
+    // Retrieve user by email
+    public User getByEmail(String email) {
+        return userRepository.findByEmail(email)
                 .orElseThrow(() -> new AppException(ErrorApp.EMAIL_NOT_EXISTED));
     }
 
-    public boolean existingEmail(String email){
+    // Check if email exists
+    public boolean existingEmail(String email) {
         return userRepository.existsByEmail(email);
     }
 
+    // Retrieve paginated users
     @PreAuthorize("hasRole('ADMIN')")
-    public Page<User> getPageUser(Integer pageNum, Integer size, String sortField, String keyword){
-        Sort sort = sortField != null ? Sort.by(sortField).ascending() : Sort.unsorted();
+    public Page<UserResponse> getPageUser(Integer pageNum, Integer size, String sortField, String keyword) {
+        Sort sort = (sortField != null) ? Sort.by(sortField).ascending() : Sort.unsorted();
         Pageable pageable = PageRequest.of(pageNum, size, sort);
 
-        if(keyword == null){
-            return userRepository.findAll(pageable);
+        Page<User> userPage;
+        if (keyword == null) {
+            userPage = userRepository.findAll(pageable);
         } else {
-            return userRepository.findAllUser(keyword, pageable);
+            userPage = userRepository.findAllUser(keyword, pageable);
         }
+
+        return userPage.map(userMapper::toUserResponse);
     }
 
+    // Update user
     @PreAuthorize("hasRole('ADMIN') or #id == principal.claims['data']['id']")
-    public User update(Integer id, UpdateUserRequest request){
+    public UserResponse update(Integer id, UpdateUserRequest request) {
         User prevUser = getById(id);
-        if(!prevUser.getEmail().equals(request.getEmail()) && existingEmail(request.getEmail())){
+
+        if (!prevUser.getEmail().equals(request.getEmail()) && existingEmail(request.getEmail())) {
             throw new AppException(ErrorApp.EMAIL_EXISTED);
         }
 
@@ -103,24 +121,26 @@ public class UserService {
         newUser.setUsername(prevUser.getUsername());
         newUser.setPassword(prevUser.getPassword());
 
-        return userRepository.save(newUser);
+        return userMapper.toUserResponse(userRepository.save(newUser));
     }
 
+    // Delete user
     @PreAuthorize("hasRole('ADMIN')")
-    public void delete(Integer id){
+    public void delete(Integer id) {
         User user = getById(id);
-
-        userRepository.deleteById(id);
+        userRepository.delete(user);
     }
 
+    // Change password
     @PreAuthorize("#id == principal.claims['data']['id']")
-    public void changePassword(Integer id, ChangePasswordRequest request){
+    public void changePassword(Integer id, ChangePasswordRequest request) {
         User user = getById(id);
-        if(passwordEncoder.matches(request.getOldPassword(), user.getPassword())){
-            user.setPassword(passwordEncoder.encode(request.getNewPassword()));
-            userRepository.save(user);
-        } else {
+
+        if (!passwordEncoder.matches(request.getOldPassword(), user.getPassword())) {
             throw new AppException(ErrorApp.OLD_PASSWORD_INCORRECT);
         }
+
+        user.setPassword(passwordEncoder.encode(request.getNewPassword()));
+        userRepository.save(user);
     }
 }
