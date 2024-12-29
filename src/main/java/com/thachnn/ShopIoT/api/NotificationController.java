3444f56d4690aa9d5eea29acd60882975eb3c86e
thachnn <thach11052002@gmail.com>
2024-12-29 20:11:27 +0700

    - Updated `/notifications` API to support pagination with `number`, `size`, and `lastTimestamp` parameters for efficient fetching of notifications. - Added `Slice<Notification>` handling to provide paginated responses with `hasPrevious` and `hasNext` indicators. - Adjusted the API endpoint for marking notifications as viewed to `/notifications/viewed/{id}`

--- src/main/java/com/thachnn/ShopIoT/api/NotificationController.java
+++ src/main/java/com/thachnn/ShopIoT/api/NotificationController.java
@@ -1,34 +1,52 @@
 package com.thachnn.ShopIoT.api;
 
 import com.thachnn.ShopIoT.dto.response.ApiResponse;
-import com.thachnn.ShopIoT.service.NotificationNewOrderService;
+import com.thachnn.ShopIoT.model.Notification;
+import com.thachnn.ShopIoT.service.NotificationService;
+import com.thachnn.ShopIoT.util.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Slice;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @RestController
-@RequestMapping("/notification")
+@RequestMapping("/notifications")
 public class NotificationController {
 
     @Autowired
-    NotificationNewOrderService notificationNewOrderService;
+    NotificationService notificationService;
 
     @GetMapping
-    public ResponseEntity<?> getAll() {
+    public ResponseEntity<?> getAll(
+            @RequestParam(value = "number", defaultValue = "1") Integer number,
+            @RequestParam(value = "size", defaultValue = "5") Integer size,
+            @RequestParam(value = "lastTimestamp", required = false) LocalDateTime lastTimestamp
+    ){
+        Slice<Notification> slice = notificationService.getAllNotification(lastTimestamp,number-1, size);
+        List<Notification> notifications = slice.getContent();
+
         return ResponseEntity.ok(
                 ApiResponse.builder()
                         .success(true)
-                        .content(notificationNewOrderService.getAllNotification())
+                        .content(notifications)
+                        .pageDetails(
+                                PageInfo.builder()
+                                        .hasPrevious(slice.hasPrevious())
+                                        .hasNext(slice.hasNext())
+                                        .build()
+                        )
                         .build()
         );
     }
 
-    @PutMapping("/viewedNotification/{id}")
+    @PutMapping("/viewed/{id}")
     public ResponseEntity<?> changeViewed(
             @PathVariable("id") String id
     ){
-        notificationNewOrderService.viewedNotification(id);
+        notificationService.viewedNotification(id);
         return ResponseEntity.ok(
                 ApiResponse.builder()
                 .success(true)
